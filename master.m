ff526ad523e922d7498ea3a12e2f02f67ad846f2
RaquelLeandra <raquelpa93@gmail.com>
2016-09-27 13:28:58 +0200

    Master+NewtonRaphson
    Añado newton Raphson

--- master.m
+++ master.m
@@ -1,7 +1,24 @@
+clear all 
 % Calculamos Newton con tolerancia
 puntoInicial = 3;
 numeroIteraciones = 10;
 tolerancia = 0.000001;
 f = @(x)f1(x);
 df = @(x)df1(x);
-[resultado, residuo] = newton(puntoInicial,numeroIteraciones, tolerancia, f, df);
+%[resultado, residuo] = newton(puntoInicial,numeroIteraciones, tolerancia, f, df);
+
+
+%%%%%%%%Práctica Newton-Raphson
+
+%Creamoss la funcion que evalua f( residu de f )
+
+%Calculamos el jacobiano
+syms X Y Z;
+
+cosa = [X;Y;Z];    
+
+jacobian(residu(cosa),cosa);
+
+%2 hacemos trampa y calculamos el resultado con fsolve
+
+resul = fsolve(@residu, [0;0;0]);
