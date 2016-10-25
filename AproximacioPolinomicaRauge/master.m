ac6b9ba37092b69072e58292d5523af4cbb97571
raquel <raquelpa93@gmail.com>
2016-10-25 13:08:30 +0200

    Apartado A terminado

--- AproximacioPolinomicaRauge/master.m
+++ AproximacioPolinomicaRauge/master.m
@@ -1,9 +1,14 @@
 %% PROBLEMA 1: INTERPOLAR ourfunction CON EL 
-%%CRITERIO DE INTERPOLACION USANDO n+1 = 3,5,7,9
-n = 3;
-p = interpolation(n,@(x)ourfunction(x), -1, 1);
-
+%% CRITERIO DE INTERPOLACION USANDO n+1 = 3,5,7,9
 i = linspace(-1,1,100);
-e = evaluatePoly(p,i)
-plot(i,ourfunction(i));
-plot(i,evaluatePoly(p,i));
+y = ourfunction(i);
+
+figure
+plot(i,y);
+hold on
+for n = [3,5,7,9]
+  p = interpolation(n,@(x)ourfunction(x), -1, 1);
+  e = polyval(p,i);
+  plot(i,e); 
+end
+hold of
