55ad257d7b5aadf4177bd48beb0a1667a6ad4272
raquel <raquelpa93@gmail.com>
2016-11-22 13:04:04 +0100

    NewtonCotes Funciona

--- 5.Integracion Numerica/master.m
+++ 5.Integracion Numerica/master.m
@@ -1,8 +1,28 @@
 a = 0;
 b = 5;
-n = 5;
-x = linspace(a,b,5);
-y = f1(x);
-w = NewtonCotes(x,a,b);
 
-valorIntegral = y*w
+eje = linspace(0,6,100);
+figure(1)
+subplot(1,2,1)
+plot(eje,f1(eje))
+
+%% Cuadratura de Newton-Cotes: 
+
+valorIntegral = [];
+for i = 2:14
+    n = i;
+    x = linspace(a,b,i);
+    y = f1(x);
+    w = NewtonCotes(x,a,b);
+    valorIntegral = [valorIntegral y*w];
+end
+
+valorExactoIntegral = exp(-a)-exp(-b) + sqrt(pi)/4*(erf(b-4)-erf(a-4));
+
+figure(1)
+subplot(1,2,2)
+plot(log(abs(valorIntegral - valorExactoIntegral)),'.-')
+hold on
+legend('NC')
+
+%% Cuadratura de Gauss-Lagendre
